47fed096dcdc34b008867a9df2bc8433fad46b7a
sberezovsky <sergey.berezovskiy@gmail.com>
2014-08-03 12:03:48 +0300

    Batch null sequence

---
+++ Steroids.Tests/Linq/BatchTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Steroids;
+
+namespace Steroids.Tests.Linq
+{
+    public class BatchTests
+    {
+        [Fact]
+        public void ShouldNotBatchNullSequence()
+        {
+            IEnumerable<object> sequence = null;
+            Assert.Throws<ArgumentNullException>(() => sequence.Batch(3));
+        }
+    }
+}
---
+++ Steroids/EnumerableExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Steroids
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<IEnumerable<T>> Batch<T>(this IEnumerable<T> source, int size)
+        {
+            throw new ArgumentNullException();
+        }
+    }
+}
